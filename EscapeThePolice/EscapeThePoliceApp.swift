0ef45a5101975f82773d966def1a539865df7bdf
AviadKorakin <aviad825@gmail.com>
2025-07-10 13:02:47 +0300

    Initial Commit

---
+++ EscapeThePolice/EscapeThePoliceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeThePoliceApp.swift
+//  EscapeThePolice
+//
+//  Created by Aviad on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct EscapeThePoliceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EscapeThePoliceTests/EscapeThePoliceTests.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeThePoliceTests.swift
+//  EscapeThePoliceTests
+//
+//  Created by Aviad on 10/07/2025.
+//
+
+import Testing
+@testable import EscapeThePolice
+
+struct EscapeThePoliceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
